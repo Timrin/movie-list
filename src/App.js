a9e57033cf30e2f74b2dc2a4a1b69f50cf9c670a
Timrin <19280360+Timrin@users.noreply.github.com>
2022-05-12 16:01:35 +0200

    Add methods for adding and removing movies
    Added methods to add and remove movies from the movies list in App
    Implemented the abillity to add movies with the MovieForm, now movies added are added to the movie list in App

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import MovieList from "./components/MovieList";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.addMovie = this.addMovie.bind(this);
+    this.removeMovie = this.removeMovie.bind(this);
     this.state = {
-      movies: [{
-        id: 1,
-        title: "Dune",
-        rating: 5
-      },
-      {
-        id: 2,
-        title: "Tenet",
-        rating: 5
-      }]
+      movies: []
     };
+  }
+
+  addMovie(title, rating) {
+    let movies = this.state.movies;
+    console.log(this.state.movies)
+    //Id of the added movie is 1 if there are no movies. Otherwise the id is the id of the last movie plus 1
+    let id = movies.length == 0 ? 1 : (this.state.movies[this.state.movies.length-1].id) + 1; 
+
+    movies.push({
+      id: id,
+      title: title,
+      rating: parseInt(rating)
+    })
+
+    this.setState({movies: movies})
+    console.log(this.state.movies)
+  }
+
+  removeMovie(id) {
   }
 
   render() {
@@ -24,7 +36,7 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <h1>Movie List</h1>
-        <MovieInputForm />
+        <MovieInputForm onAddMovie={this.addMovie}/>
         <MovieList movies={movies}/>
       </div>
     );
--- src/components/MovieInputForm.js
+++ src/components/MovieInputForm.js
@@ -1,11 +1,11 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 
-function handleSubmit(e, rating, title, setTitle, setRating) {
+function handleSubmit(e, rating, title, setTitle, setRating, addMovie) {
     e.preventDefault()
-    
+
     if (validateForm(rating, title)) {
         //Add movie
-        console.log(`Added Movie: title: ${title}, rating: ${rating}`);
+        addMovie(title, rating);
 
         //Clear form
         setTitle('');
@@ -14,49 +14,48 @@ function handleSubmit(e, rating, title, setTitle, setRating) {
 };
 
 function validateForm(rating, title) {
-        let isFormValid = true;
+    let isFormValid = true;
 
-        //Validate form input
-        if (title == "") {
-            window.alert("Please provide the title of the movie")
-            isFormValid = false;
-        } else if (rating < 1 || rating > 5) {
-            window.alert("Please provide your rating for the movie")
+    //Validate form input
+    if (title == "") {
+        window.alert("Please provide the title of the movie")
+        isFormValid = false;
+    } else if (rating < 1 || rating > 5) {
+        window.alert("Please provide your rating for the movie")
+        isFormValid = false;
+    }
 
-            isFormValid = false;
-        }
-
-        return isFormValid;
+    return isFormValid;
 }
 
-function MovieInputForm() {
+function MovieInputForm(props) {
     const [rating, setRating] = useState(0);
     const [title, setTitle] = useState('');
 
     return (
-        <form id="add-movie-form" onSubmit={e => {handleSubmit(e, rating, title, setTitle, setRating)}}>
-        <fieldset>
-            <legend>Lägg till en film</legend>
+        <form id="add-movie-form" onSubmit={e => { handleSubmit(e, rating, title, setTitle, setRating, props.onAddMovie) }}>
+            <fieldset>
+                <legend>Lägg till en film</legend>
 
-            <label htmlFor="title-field">Titel:</label>
-            <input value={title} type="text" id="title-field" className="form-control" onChange={e => {setTitle(e.target.value)}}/>
+                <label htmlFor="title-field">Titel:</label>
+                <input value={title} type="text" id="title-field" className="form-control" onChange={e => { setTitle(e.target.value) }} />
 
-            <label htmlFor="rating-field">Betyg:</label>
+                <label htmlFor="rating-field">Betyg:</label>
 
-            <select value={rating} type="text" id="rating-field" className="form-control" onChange={e => {setRating(e.target.value)}}>
-                <option value="0">Välj betyg här...</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
+                <select value={rating} type="text" id="rating-field" className="form-control" onChange={e => { setRating(e.target.value) }}>
+                    <option value="0">Välj betyg här...</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
 
-            <input type="submit" className="btn btn-success mt-3" value="Spara film"/>
+                <input type="submit" className="btn btn-success mt-3" value="Spara film" />
 
-        </fieldset>
-    </form>
+            </fieldset>
+        </form>
     );
-  }
-  
-  export default MovieInputForm;
+}
+
+export default MovieInputForm;
