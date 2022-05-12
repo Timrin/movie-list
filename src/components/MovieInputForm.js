9130d2d587f78f6cf581fd2236936175bcfa677a
Timrin <19280360+Timrin@users.noreply.github.com>
2022-05-12 14:35:24 +0200

    Add functionallity to MovieInputForm
    Continued implementing the MovieInputForm
    The form state is now managed
    The form now gets validated when data is submitted

--- src/components/MovieInputForm.js
+++ src/components/MovieInputForm.js
@@ -1,15 +1,49 @@
+import React, {useState, useRef} from 'react';
+
+function handleSubmit(e, rating, title, setTitle, setRating) {
+    e.preventDefault()
+    
+    if (validateForm(rating, title)) {
+        //Add movie
+        console.log(`Added Movie: title: ${title}, rating: ${rating}`);
+
+        //Clear form
+        setTitle('');
+        setRating(0);
+    }
+};
+
+function validateForm(rating, title) {
+        let isFormValid = true;
+
+        //Validate form input
+        if (title == "") {
+            window.alert("Please provide the title of the movie")
+            isFormValid = false;
+        } else if (rating < 1 || rating > 5) {
+            window.alert("Please provide your rating for the movie")
+
+            isFormValid = false;
+        }
+
+        return isFormValid;
+}
+
 function MovieInputForm() {
+    const [rating, setRating] = useState(0);
+    const [title, setTitle] = useState('');
+
     return (
-        <form id="add-movie-form">
+        <form id="add-movie-form" onSubmit={e => {handleSubmit(e, rating, title, setTitle, setRating)}}>
         <fieldset>
             <legend>Lägg till en film</legend>
 
             <label htmlFor="title-field">Titel:</label>
-            <input type="text" id="title-field" className="form-control"/>
+            <input value={title} type="text" id="title-field" className="form-control" onChange={e => {setTitle(e.target.value)}}/>
 
             <label htmlFor="rating-field">Betyg:</label>
 
-            <select type="text" id="rating-field" className="form-control">
+            <select value={rating} type="text" id="rating-field" className="form-control" onChange={e => {setRating(e.target.value)}}>
                 <option value="0">Välj betyg här...</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
