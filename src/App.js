31350b701117a630b2b75410ac57bd2da2851754
Timrin <19280360+Timrin@users.noreply.github.com>
2022-05-12 15:21:26 +0200

    Convert App to a class component
    Converted the App component to a class component
    Lifted movie data from the MovieList to the App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import React from 'react';
 import MovieInputForm from "./components/MovieInputForm";
 import MovieList from "./components/MovieList";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Movie List</h1>
-      <MovieInputForm />
-      <MovieList />
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [{
+        id: 1,
+        title: "Dune",
+        rating: 5
+      },
+      {
+        id: 2,
+        title: "Tenet",
+        rating: 5
+      }]
+    };
+  }
 
-export default App;
+  render() {
+    const movies = this.state.movies;
+    return (
+      <div className="App">
+        <h1>Movie List</h1>
+        <MovieInputForm />
+        <MovieList movies={movies}/>
+      </div>
+    );
+  }
+}
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,24 +1,15 @@
 import './MovieList.css'
 
-function MovieList() {
-    //Dummy values
-    const movies = [{
-        id: 1,
-        title: "Dune",
-        rating: 5
-    },
-    {
-        id: 2,
-        title: "Tenet",
-        rating: 5
-    }];
+function MovieList(props) {
+    const movies = props.movies;
 
+    // The li elements key attribute is the movie id of the movie that is represented by the element.
     // The delete img element always has the key 1, the stars have the key 2-6 depending on how many there are
     const listItems = movies.map((movie) =>
         <li key={movie.id} data-grade={movie.rating} data-title={movie.title}>
             {movie.title}
             <img key={1} src="images/delete.png" alt="Delete movie" className="delete-movie-icon"></img>
-            {Array.from({length: movie.rating}, (_, index) => <img key={index+1} src="images/star.png" alt="Star"></img>)}
+            {Array.from({length: movie.rating}, (_, index) => <img key={index + 1} src="images/star.png" alt="Star"></img>)}
         </li>
     );
 
