81252d6d8b9f31caa9046e7421e0b1af94f50616
Timrin <19280360+Timrin@users.noreply.github.com>
2022-05-15 18:16:16 +0200

    Refactor MovieInputForm to a function
    Rewrote MovieInputForm, it now uses ref instead of state. This was
    just to try out using ref, not sure which is more appropriote. Also
    converted MovieInputForm to a function instead of a class.

--- src/components/MovieInputForm.js
+++ src/components/MovieInputForm.js
@@ -1,48 +1,48 @@
-import React, { useState, useRef } from 'react';
-
-function handleSubmit(e, rating, title, setTitle, setRating, addMovie) {
-    e.preventDefault()
-
-    if (validateForm(rating, title)) {
-        //Add movie
-        addMovie(title, rating);
-
-        //Clear form
-        setTitle('');
-        setRating(0);
-    }
-};
-
-function validateForm(rating, title) {
-    let isFormValid = true;
-
-    //Validate form input
-    if (title == "") {
-        window.alert("Please provide the title of the movie")
-        isFormValid = false;
-    } else if (rating < 1 || rating > 5) {
-        window.alert("Please provide your rating for the movie")
-        isFormValid = false;
-    }
-
-    return isFormValid;
-}
+import React, { useRef } from 'react';
 
 function MovieInputForm(props) {
-    const [rating, setRating] = useState(0);
-    const [title, setTitle] = useState('');
+    const titleRef = useRef();
+    const ratingRef = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    
+        if (validateForm()) {
+            //Add movie
+            props.onAddMovie(titleRef.current.value, ratingRef.current.value);
+    
+            //Clear form
+            titleRef.current.value = "";
+            ratingRef.current.value = 0;
+        }
+    };
+
+    const validateForm = () => {
+        let isFormValid = true;
+    
+        //Validate form input
+        if (titleRef.current.value == "") {
+            window.alert("Please provide the title of the movie")
+            isFormValid = false;
+        } else if (ratingRef.current.value < 1 || ratingRef.current.value > 5) {
+            window.alert("Please provide your rating for the movie")
+            isFormValid = false;
+        }
+    
+        return isFormValid;
+    }
 
     return (
-        <form id="add-movie-form" onSubmit={e => { handleSubmit(e, rating, title, setTitle, setRating, props.onAddMovie) }}>
+        <form id="add-movie-form" onSubmit={e => { handleSubmit(e) }}>
             <fieldset>
                 <legend>Lägg till en film</legend>
 
                 <label htmlFor="title-field">Titel:</label>
-                <input value={title} type="text" id="title-field" className="form-control" onChange={e => { setTitle(e.target.value) }} />
+                <input ref={titleRef} type="text" id="title-field" className="form-control" />
 
                 <label htmlFor="rating-field">Betyg:</label>
 
-                <select value={rating} type="text" id="rating-field" className="form-control" onChange={e => { setRating(e.target.value) }}>
+                <select ref={ratingRef} type="text" id="rating-field" className="form-control">
                     <option value="0">Välj betyg här...</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
